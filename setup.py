82364edc94af6a6c1d00cf8631e5011aebd60d35
Matthew Honnibal <matthew@honnibal.com>
2014-09-25 03:20:32 +0200

    * Change language to C from C++

--- setup.py
+++ setup.py
@@ -10,19 +10,19 @@ from os.path import splitext
 
 from distutils.sysconfig import get_python_inc
 
-exts = [Extension("cymem.cymem", ["cymem/cymem.pyx"], language="c++")]
+exts = [Extension("cymem.cymem", ["cymem/cymem.pyx"])]
 
 setup(
     ext_modules=cythonize(exts),
     name="cymem",
     packages=["cymem"],
-    version="0.3",
+    version="0.61",
     author="Matthew Honnibal",
     author_email="honnibal@gmail.com",
     url="http://github.com/syllog1sm/cymem",
-    package_data={"cymem": ["*.pxd", "*.pyx", "*.cpp"]},
-    description="""Manage calls to malloc/free through Cython""",
-    classifiers=3
+    package_data={"cymem": ["*.pxd", "*.pyx", "*.c"]},
+    description="""Manage calls to calloc/free through Cython""",
+    classifiers=[
                 'Environment :: Console',
                 'Operating System :: OS Independent',
                 'Intended Audience :: Science/Research',
