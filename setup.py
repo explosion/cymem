7fa698f185b3371bfebdb3feb08cd6527f829547
Matthew Honnibal <honnibal@gmail.com>
2014-10-21 19:16:20 +1100

    * Increment version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     ext_modules=cythonize(exts),
     name="cymem",
     packages=["cymem"],
-    version="0.9",
+    version="1.0",
     author="Matthew Honnibal",
     author_email="honnibal@gmail.com",
     url="http://github.com/syllog1sm/cymem",
