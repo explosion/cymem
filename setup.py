cab04fca914b06548ad551a8fb04a41c2672291f
Henning Peters <pete@dexterslab.de>
2016-02-25 09:37:50 +0100

    sync setup.py/cythonize with other spacy projects

--- setup.py
+++ setup.py
@@ -1,35 +1,127 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from __future__ import print_function
+import os
+import shutil
+import subprocess
+import sys
+import contextlib
+from distutils.command.build_ext import build_ext
+from distutils.sysconfig import get_python_inc
 
 try:
-    from Cython.Build import cythonize
-    from Cython.Distutils import Extension
-    exts = cythonize([Extension("cymem.cymem", ["cymem/cymem.pyx"])])
+    from setuptools import Extension, setup
 except ImportError:
-    from distutils.extension import Extension
-    exts = [Extension("cymem.cymem", ["cymem/cymem.c"])]
+    from distutils.core import Extension, setup
 
 
-import sys
-import os
-from os.path import splitext
+PACKAGES = [
+    'cymem',
+    'cymem.tests']
+MOD_NAMES = [
+    'cymem.cymem']
 
 
-setup(
-    ext_modules=exts,
+# By subclassing build_extensions we have the actual compiler that will be used which is really known only after finalize_options
+# http://stackoverflow.com/questions/724664/python-distutils-how-to-get-a-compiler-that-is-going-to-be-used
+compile_options =  {'msvc'  : ['/Ox', '/EHsc'],
+                    'other' : ['-O3', '-Wno-strict-prototypes', '-Wno-unused-function']}
+link_options    =  {'msvc'  : [],
+                    'other' : []}
 
-    name="cymem",
-    packages=["cymem", "cymem.tests"],
-    version="1.31.0",
-    author="Matthew Honnibal",
-    author_email="honnibal@gmail.com",
-    url="http://github.com/syllog1sm/cymem",
-    package_data={"cymem": ["*.pxd", "*.pyx", "*.c"]},
-    description="""Manage calls to calloc/free through Cython""",
-    classifiers=[
-                'Environment :: Console',
-                'Operating System :: OS Independent',
-                'Intended Audience :: Science/Research',
-                'Programming Language :: Cython',
-                'Topic :: Scientific/Engineering'],
-)
+class build_ext_options:
+    def build_options(self):
+        for e in self.extensions:
+            e.extra_compile_args = compile_options.get(
+                self.compiler.compiler_type, compile_options['other'])
+        for e in self.extensions:
+            e.extra_link_args = link_options.get(
+                self.compiler.compiler_type, link_options['other'])
+
+
+class build_ext_subclass(build_ext, build_ext_options):
+    def build_extensions(self):
+        build_ext_options.build_options(self)
+        build_ext.build_extensions(self)
+
+
+def generate_cython(root, source):
+    print('Cythonizing sources')
+    p = subprocess.call([sys.executable,
+                         os.path.join(root, 'bin', 'cythonize.py'),
+                         source])
+    if p != 0:
+        raise RuntimeError('Running cythonize failed')
+
+
+def is_source_release(path):
+    return os.path.exists(os.path.join(path, 'PKG-INFO'))
+
+
+def clean(path):
+    for name in MOD_NAMES:
+        name = name.replace('.', '/')
+        for ext in ['.so', '.html', '.cpp', '.c']:
+            file_path = os.path.join(path, name + ext)
+            if os.path.exists(file_path):
+                os.unlink(file_path)
+
+
+@contextlib.contextmanager
+def chdir(new_dir):
+    old_dir = os.getcwd()
+    try:
+        os.chdir(new_dir)
+        sys.path.insert(0, new_dir)
+        yield
+    finally:
+        del sys.path[0]
+        os.chdir(old_dir)
+
+
+def setup_package():
+    root = os.path.abspath(os.path.dirname(__file__))
+
+    if len(sys.argv) > 1 and sys.argv[1] == 'clean':
+        return clean(root)
+
+    with chdir(root):
+        about = {}
+        with open(os.path.join(root, 'cymem', 'about.py')) as f:
+            exec(f.read(), about)
+
+        include_dirs = [
+            get_python_inc(plat_specific=True)]
+
+        ext_modules = []
+        for mod_name in MOD_NAMES:
+            mod_path = mod_name.replace('.', '/') + '.cpp'
+            ext_modules.append(
+                Extension(mod_name, [mod_path],
+                    language='c++', include_dirs=include_dirs))
+
+        if not is_source_release(root):
+            generate_cython(root, 'cymem')
+
+        setup(
+            name=about['__name__'],
+            packages=PACKAGES,
+            package_data={'': ['*.pyx', '*.pxd']},
+            description=about['__summary__'],
+            author=about['__author__'],
+            author_email=about['__email__'],
+            version=about['__version__'],
+            url=about['__uri__'],
+            license=about['__license__'],
+            ext_modules=ext_modules,
+            classifiers=['Environment :: Console',
+                         'Operating System :: OS Independent',
+                         'Intended Audience :: Science/Research',
+                         'Programming Language :: Cython',
+                         'Topic :: Scientific/Engineering'],
+            cmdclass = {
+                'build_ext': build_ext_subclass},
+        )
+
+
+if __name__ == '__main__':
+    setup_package()
