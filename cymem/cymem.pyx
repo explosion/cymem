1a877ce2ee09fce51b26e1c9eea7ad5eb64ae9b7
Matthew Honnibal <honnibal+gh@gmail.com>
2017-09-16 20:48:40 +0200

    Fix error message

--- cymem/cymem.pyx
+++ cymem/cymem.pyx
@@ -65,7 +65,7 @@ cdef class Pool:
         """
         cdef void* p = self.pymalloc.malloc(number * elem_size)
         if p == NULL:
-            raise MemoryError("Error assigning %d bytes" % number * elem_size)
+            raise MemoryError("Error assigning %d bytes" % (number * elem_size))
         memset(p, 0, number * elem_size)
         self.addresses[<size_t>p] = number * elem_size
         self.size += number * elem_size
