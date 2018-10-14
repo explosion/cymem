fa8e5bb9104bd8a27918103f0e5b39fb3f12bc6f
Ines Montani <ines@ines.io>
2018-10-14 16:40:32 +0200

    Add wheel to setup_requires

--- setup.py
+++ setup.py
@@ -118,6 +118,7 @@ def setup_package():
             url=about['__uri__'],
             license=about['__license__'],
             ext_modules=ext_modules,
+            setup_requires=['wheel>=0.32.0,<0.33.0'],
             classifiers=[
                 'Environment :: Console',
                 'Intended Audience :: Developers',
