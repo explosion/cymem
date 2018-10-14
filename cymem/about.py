2cb2c207085ba4297b71d70a230a021786fbb100
Matthew Honnibal <honnibal+gh@gmail.com>
2018-10-14 17:35:57 +0200

    Set version to 2.0.1

--- cymem/about.py
+++ cymem/about.py
@@ -4,7 +4,7 @@
 # https://github.com/pypa/warehouse/blob/master/warehouse/__about__.py
 
 __title__ = 'cymem'
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 __summary__ = 'Manage calls to calloc/free through Cython'
 __uri__ = 'https://github.com/explosion/cymem'
 __author__ = 'Matthew Honnibal'
