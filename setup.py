a96800e98045de92318c86d17bb03bc0fd547832
Adriane Boyd <adrianeboyd@gmail.com>
2020-11-03 09:40:39 +0100

    Add python 3.9 classifier (#21)

--- setup.py
+++ setup.py
@@ -139,6 +139,7 @@ def setup_package():
                 "Programming Language :: Python :: 3.6",
                 "Programming Language :: Python :: 3.7",
                 "Programming Language :: Python :: 3.8",
+                "Programming Language :: Python :: 3.9",
                 "Topic :: Scientific/Engineering",
             ],
             cmdclass={"build_ext": build_ext_subclass},
