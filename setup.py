364495cbc5ec2ab59c522f8c785683603d1b2500
Adriane Boyd <adrianeboyd@gmail.com>
2021-10-22 09:08:55 +0200

    Set version to v2.0.6, extend to python 3.10 (#32)

--- setup.py
+++ setup.py
@@ -120,6 +120,7 @@ def setup_package():
                 "Programming Language :: Python :: 3.7",
                 "Programming Language :: Python :: 3.8",
                 "Programming Language :: Python :: 3.9",
+                "Programming Language :: Python :: 3.10",
                 "Topic :: Scientific/Engineering",
             ],
             cmdclass={"build_ext": build_ext_subclass},
