5ece8cfdf2eb399df85afafb8c5981e1dbac58ca
Matthew Honnibal <honnibal+gh@gmail.com>
2024-07-22 11:53:44 +0200

    Bump version

--- cymem/about.py
+++ cymem/about.py
@@ -1,5 +1,5 @@
 __title__ = "cymem"
-__version__ = "2.0.9a1"
+__version__ = "2.0.9a2"
 __summary__ = "Manage calls to calloc/free through Cython"
 __uri__ = "https://github.com/explosion/cymem"
 __author__ = "Matthew Honnibal"
