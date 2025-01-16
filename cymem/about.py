956769a193641bcaefc9e0086829c87dcb20f564
Matthew Honnibal <honnibal+gh@gmail.com>
2025-01-16 21:56:45 +0100

    Increment version

--- cymem/about.py
+++ cymem/about.py
@@ -1,5 +1,5 @@
 __title__ = "cymem"
-__version__ = "2.0.10"
+__version__ = "2.0.11"
 __summary__ = "Manage calls to calloc/free through Cython"
 __uri__ = "https://github.com/explosion/cymem"
 __author__ = "Matthew Honnibal"
